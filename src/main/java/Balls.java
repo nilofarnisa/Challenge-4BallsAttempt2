3ba3203bf39041fafe09ce4e176425294bed038f
Nilofar Nisa <nilofarnisa28@gmail.com>
2021-02-25 15:21:01 +0530

    Code Modified

--- src/main/java/Balls.java
+++ src/main/java/Balls.java
@@ -1,10 +1,26 @@
-public class Balls{
+import processing.core.PApplet;
+
+public class Balls extends PApplet {
     public int height;
     public int speed;
+    private PApplet ball;
+    public int DIAMETER=10;
+    public static int x=0;
 
-    public Balls(int height,int speed) {
+    public Balls(PApplet ball,int height,int speed) {
+        this.ball=ball;
         this.height = height;
         this.speed = speed;
+    }
+
+    public static void ballSpeed()
+    {
+        x++;
+    }
+
+    public void drawCircle()
+    {
+        ball.ellipse(x*speed,height,DIAMETER,DIAMETER);
     }
 }
 
--- src/main/java/ProcessingOOPAttempt.java
+++ src/main/java/ProcessingOOPAttempt.java
@@ -4,9 +4,12 @@ public class ProcessingOOPAttempt extends PApplet{
 
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
-    public static final int DIAMETER = 10;
     public static final int SPEED = 1;
-    int x = 0;
+
+    Balls b1 = new Balls(this,HEIGHT/5,SPEED);
+    Balls b2 = new Balls(this,HEIGHT*2/5,SPEED*2);
+    Balls b3 = new Balls(this,HEIGHT*3/5,SPEED*3);
+    Balls b4 = new Balls(this,HEIGHT*4/5,SPEED*4);
 
     public static void main(String[] args) {
         PApplet.main("ProcessingOOPAttempt",args);
@@ -20,19 +23,10 @@ public class ProcessingOOPAttempt extends PApplet{
 
     @Override
     public void draw() {
-        Balls b1 = new Balls(HEIGHT/5,SPEED);
-        Balls b2 = new Balls(HEIGHT*2/5,SPEED*2);
-        Balls b3 = new Balls(HEIGHT*3/5,SPEED*3);
-        Balls b4 = new Balls(HEIGHT*4/5,SPEED*4);
-        drawCircle(b1.speed,b1.height);
-        drawCircle(b2.speed,b2.height);
-        drawCircle(b3.speed,b3.height);
-        drawCircle(b4.speed,b4.height);
-        x++;
-    }
-
-    private void drawCircle(int speedOfBall,int heightFromTop)
-    {
-        ellipse(x*speedOfBall,heightFromTop,DIAMETER,DIAMETER);
+        Balls.ballSpeed();
+        b1.drawCircle();
+        b2.drawCircle();
+        b3.drawCircle();
+        b4.drawCircle();
     }
 }
